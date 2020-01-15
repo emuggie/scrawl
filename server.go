e35434144d48142ba8174756bf845e00ba6f2598
emuggie <emuggie@gmail.com>
2020-01-15 10:19:03 +0900

    added index path

--- server.go
+++ server.go
@@ -19,6 +19,7 @@ type Memo struct {
 }
 
 var idx = 1
+var memos = []Memo{}
 
 func getIdx() int {
 	if idx >= math.MaxUint32 {
@@ -28,89 +29,124 @@ func getIdx() int {
 	return idx
 }
 
+func setHeaders(res http.ResponseWriter) {
+	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func onError(res http.ResponseWriter) {
+	err := recover()
+	if err == nil {
+		return
+	}
+	fmt.Println(err)
+	res.WriteHeader(http.StatusInternalServerError)
+	res.Write([]byte("500"))
+	return
+}
+
+func onPost(res http.ResponseWriter, req *http.Request) {
+	var memo Memo
+	json.NewDecoder(req.Body).Decode(&memo)
+
+	resp := struct {
+		Status  string `json:"status"`
+		Id      int    `json:"id"`
+		Message string `json:"message"`
+	}{
+		"OK",
+		memo.Id,
+		"",
+	}
+	enc := json.NewEncoder(res)
+
+	if memo.Text == "" {
+		resp.Status = "ERROR"
+		resp.Message = "Emtpy text"
+		enc.Encode(resp)
+		return
+	}
+	// Insert
+	if memo.Id == 0 {
+		memo.Modified = time.Now()
+		memo.Id = getIdx()
+		memos = append(memos, memo)
+		resp.Id = memo.Id
+		enc.Encode(resp)
+		return
+	}
+	// Update
+	for _, each := range memos {
+		if each.Id != memo.Id {
+			continue
+		}
+		each.Text = memo.Text
+		each.Modified = time.Now()
+		enc.Encode(resp)
+		return
+	}
+}
+
+// Delete
+func onDelete(res http.ResponseWriter, req *http.Request) {
+	var memo Memo
+	json.NewDecoder(req.Body).Decode(&memo)
+	fmt.Println("DELETE", memo.Id, memo.Text)
+	resp := struct {
+		Status  string `json:"status"`
+		Id      int    `json:"id"`
+		Message string `json:"message"`
+	}{
+		"OK",
+		memo.Id,
+		"",
+	}
+	enc := json.NewEncoder(res)
+
+	if memo.Id == 0 {
+		resp.Status = "ERROR"
+		resp.Message = "Invalid Id"
+		enc.Encode(resp)
+		return
+	}
+
+	for idx, each := range memos {
+		if each.Id != memo.Id {
+			continue
+		}
+		memos = append(memos[:idx], memos[idx+1:]...)
+		enc.Encode(resp)
+		return
+	}
+}
+
 func main() {
 	log.Println("Start Server")
-	memos := []Memo{}
-
 	var mux = http.NewServeMux()
 
 	mux.HandleFunc("/memos", func(res http.ResponseWriter, req *http.Request) {
-		log.Println("Get Memos")
-		defer func() {
-			err := recover()
-			if err == nil {
-				return
-			}
-			fmt.Println(err)
-			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("500 - Something bad happened!"))
-		}()
-
+		setHeaders(res)
+		defer onError(res)
 		enc := json.NewEncoder(res)
-		res.Header().Set("Content-Type", "application/json")
-		res.Header().Set("Access-Control-Allow-Origin", "*")
 		enc.Encode(memos)
 	})
 
 	mux.HandleFunc("/memo", func(res http.ResponseWriter, req *http.Request) {
-		//Catch
-		defer func() {
-			err := recover()
-			if err == nil {
-				return
-			}
-			fmt.Println(err)
-			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte("500 - Something bad happened!"))
-		}()
+		setHeaders(res)
+		defer onError(res)
 
-		res.Header().Set("Content-Type", "application/json")
-		res.Header().Set("Access-Control-Allow-Origin", "*")
-
-		log.Println("Requesting", req.Method)
 		if req.Method == "OPTIONS" {
 			res.WriteHeader(http.StatusOK)
 			return
 		}
-		var memo Memo
-		json.NewDecoder(req.Body).Decode(&memo)
 
 		if req.Method == "POST" {
-			fmt.Println("POST", memo.Id, memo.Text)
-			if memo.Text == "" {
-				fmt.Fprintf(res, "{\"status\":\"error\"}")
-				return
-			}
-			if memo.Id == 0 {
-				//new
-				memo.Modified = time.Now()
-				memo.Id = getIdx()
-				memos = append(memos, memo)
-				fmt.Fprintf(res, "{\"status\":\"ok\",\"id\":"+strconv.Itoa(memo.Id)+"}")
-				return
-			}
-			for _, each := range memos {
-				if each.Id == memo.Id {
-					each.Text = memo.Text
-					each.Modified = time.Now()
-					break
-				}
-			}
-			fmt.Fprintf(res, "{\"status\":\"ok\",\"id\":"+strconv.Itoa(memo.Id)+"}")
+			onPost(res, req)
 			return
-		} else if req.Method == "DELETE" {
-			fmt.Println("DELETE", memo.Id, memo.Text)
-			if memo.Id == 0 {
-				//new
-				return
-			}
-			for idx, each := range memos {
-				if each.Id == memo.Id {
-					memos = append(memos[:idx], memos[idx+1:]...)
-					break
-				}
-			}
-			fmt.Fprintf(res, "{\"status\":\"ok\",\"id\":"+strconv.Itoa(memo.Id)+"}")
+		}
+
+		if req.Method == "DELETE" {
+			onDelete(res, req)
 			return
 		}
 	})
@@ -118,6 +154,10 @@ func main() {
 	// Static resource serving
 	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		filePath := strings.Trim(req.URL.Path, "/")
+		// "/ for index.html"
+		if filePath == "" {
+			filePath = "index.html"
+		}
 		contents, err := ioutil.ReadFile(filePath)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
